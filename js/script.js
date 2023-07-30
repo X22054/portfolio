9228f7e451f62ef8af441decc34d70b2bf7ee456
X22054 <x22054xx@aitech.ac.jp>
2023-07-30 23:30:06 +0900

    変更0730

--- js/script.js
+++ js/script.js
@@ -19,81 +19,17 @@ $(function () {
 
 });
 
-//スライド
-$('.slider').slick({
-  autoplay: true,//自動的に動き出すか。初期値はfalse。
-  autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
-  speed:1000,//スライドの動きのスピード。初期値は300。
-  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-  slidesToShow: 1,//スライドを画面に3枚見せる
-  slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-  arrows: true,//左右の矢印あり
-  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-  dots: true,//下部ドットナビゲーションの表示
-      pauseOnFocus: false,//フォーカスで一時停止を無効
-      pauseOnHover: false,//マウスホバーで一時停止を無効
-      pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
-  });
-
-//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
-$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
-  $('.slider').slick('slickPlay');
-});
 
 //ハンバーガーメニュー
-$(function () {
-  $(".hamBtn").on("click", function () {
-    $(".hamBtn").toggleClass("close");
-    $(".nav-wrapper").toggleClass("fade");
-    $("body").toggleClass("noscroll");
-  });
-});
 
-// https://zero-plus.io/media/nav-hamburger/
-const ham = document.querySelector('#js-hamburger');
-const nav = document.querySelector('#js-nav');
-
-ham.addEventListener('click', function () {
-
-  ham.classList.toggle('active');
-  nav.classList.toggle('active');
+const ham = $('#js-hamburger');
+const nav = $('#js-nav');
+ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+  ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+  nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
 
 });
 
-// ハンバーガーメニューをクリックした時に、メニューを閉じる
-	// <![CDATA[  <-- For SVG support
-	if ('WebSocket' in window) {
-		(function () {
-			function refreshCSS() {
-				var sheets = [].slice.call(document.getElementsByTagName("link"));
-				var head = document.getElementsByTagName("head")[0];
-				for (var i = 0; i < sheets.length; ++i) {
-					var elem = sheets[i];
-					var parent = elem.parentElement || head;
-					parent.removeChild(elem);
-					var rel = elem.rel;
-					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
-						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
-						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
-					}
-					parent.appendChild(elem);
-				}
-			}
-			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
-			var address = protocol + window.location.host + window.location.pathname + '/ws';
-			var socket = new WebSocket(address);
-			socket.onmessage = function (msg) {
-				if (msg.data == 'reload') window.location.reload();
-				else if (msg.data == 'refreshcss') refreshCSS();
-			};
-			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
-				console.log('Live reload enabled.');
-				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
-			}
-		})();
-	}
-	else {
-		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
-	}
-	// ]]>
+$('nav__items nav-items').on('click', function(event) {
+  $('header__hamburger hamburger').trigger('click');
+});
