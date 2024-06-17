0031d517c2add4760949a4a3e5f6ed0eea36cec2
X22054 <x22054xx@aitech.ac.jp>
2024-06-17 13:36:02 +0900

    s
    s

---
+++ portfolio/js/script.js
@@ -0,0 +1,52 @@
+$(function () {
+
+  //ページ内スクロール
+  var navHeight = $(".header").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate({ scrollTop: 0, }, 300);
+    return false;
+  });
+
+});
+
+
+//ハンバーガーメニュー
+
+const ham = $('#js-hamburger');
+const nav = $('#js-nav');
+ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+  ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+  nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
+
+});
+
+$('nav__items nav-items').on('click', function(event) {
+  $('header__hamburger hamburger').trigger('click');
+});
+
+//レイジーロード
+const headings = document.querySelectorAll('.popup')
+const observer = new IntersectionObserver(popupElements)
+
+function popupElements(entries){
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  })
+}
+
+headings.forEach(heading => {
+  observer.observe(heading)
+})
+
